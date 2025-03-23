a6c57a5f14b312b3301d6e6087a17c92116ca231
fostash <fausto.bonacina@gmail.com>
2025-03-23 23:05:14 +0100

    base model db, code formatter

---
+++ src/main/resources/db/migrations/V0.2__init_data.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO users(first_name,last_name,email,username,password,role) VALUES ('user1 firstname', 'user1 lastname', 'user1email@test.it','test1','password1','USER');
+INSERT INTO users(first_name,last_name,email,username,password,role) VALUES ('user2 firstname', 'user2 lastname', 'user2email@test.it','test2','password2','USER');
+INSERT INTO users(first_name,last_name,email,username,password,role) VALUES ('user3 firstname', 'user3 lastname', 'user3email@test.it','test3','password3','USER');
+INSERT INTO users(first_name,last_name,email,username,password,role) VALUES ('admin firstname', 'admin lastname', 'adminemail@test.it','admin','password','ADMIN');
