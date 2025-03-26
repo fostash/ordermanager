f7d5a1a21e6b4a4b7bf1704d1b0720c7cb621268
fostash <fausto.bonacina@gmail.com>
2025-03-26 20:28:01 +0100

    add redis and meilisearch as testcontainers

--- src/test/java/org/fbonacina/customerorders/utils/BaseITTest.java
+++ src/test/java/org/fbonacina/customerorders/utils/BaseITTest.java
@@ -1,5 +1,7 @@
 package org.fbonacina.customerorders.utils;
 
+import com.redis.testcontainers.RedisContainer;
+import io.vanslog.testcontainers.meilisearch.MeilisearchContainer;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MariaDBContainer;
@@ -10,8 +12,20 @@ public interface BaseITTest {
   @Container
   MariaDBContainer<?> mariaDb = new MariaDBContainer<>(DockerImageName.parse("mariadb:11.4"));
 
+  @Container
+  RedisContainer REDIS_CONTAINER =
+      new RedisContainer(DockerImageName.parse("redis:5.0.3-alpine")).withExposedPorts(6379);
+
+  @Container
+  MeilisearchContainer meilisearch =
+      new MeilisearchContainer(DockerImageName.parse("getmeili/meilisearch:latest"))
+          .withMasterKey("masterKey");
+
   @DynamicPropertySource
-  static void overrideProperties(DynamicPropertyRegistry registry) {
+  private static void registerContainersProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+    registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379).toString());
+
     registry.add("spring.datasource.url", mariaDb::getJdbcUrl);
     registry.add("spring.datasource.username", mariaDb::getUsername);
     registry.add("spring.datasource.password", mariaDb::getPassword);
@@ -19,5 +33,9 @@ public interface BaseITTest {
     registry.add("spring.flyway.user", mariaDb::getUsername);
     registry.add("spring.flyway.password", mariaDb::getPassword);
     registry.add("spring.flyway.driver", mariaDb::getDriverClassName);
+
+    registry.add(
+        "meilisearch.url",
+        () -> "http://" + meilisearch.getHost() + ":" + meilisearch.getMappedPort(7700));
   }
 }
