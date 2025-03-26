ea923475fb3caf5cd545fb54f158d58097a5e383
fostash <fausto.bonacina@gmail.com>
2025-03-26 20:29:18 +0100

    add products to test data

--- src/main/resources/db/migrations/V0.2__init_data.sql
+++ src/main/resources/db/migrations/V0.2__init_data.sql
@@ -3,3 +3,9 @@ INSERT INTO users(first_name,last_name,email,username,password,role) VALUES ('us
 INSERT INTO users(first_name,last_name,email,username,password,role) VALUES ('user2 firstname', 'user2 lastname', 'user2email@test.it','test2','password2','USER');
 INSERT INTO users(first_name,last_name,email,username,password,role) VALUES ('user3 firstname', 'user3 lastname', 'user3email@test.it','test3','password3','USER');
 INSERT INTO users(first_name,last_name,email,username,password,role) VALUES ('admin firstname', 'admin lastname', 'adminemail@test.it','admin','password','ADMIN');
+
+INSERT INTO products(name, description, stock_quantity, version) VALUES ("prod 1", "prod 1 descr", 20, 0);
+INSERT INTO products(name, description, stock_quantity, version) VALUES ("prod 2", "prod 2 descr", 4, 0);
+INSERT INTO products(name, description, stock_quantity, version) VALUES ("prod 3", "prod 3 descr", 67, 0);
+INSERT INTO products(name, description, stock_quantity, version) VALUES ("prod 4", "prod 4 descr", 12, 0);
+INSERT INTO products(name, description, stock_quantity, version) VALUES ("prod 5", "prod 5 descr", 5, 0);
